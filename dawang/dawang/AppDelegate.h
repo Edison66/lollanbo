36cf5d5af1fc6ef0eb48adffaf50f61476b05952
lanou3g <408037112@qq.com>
2016-06-07 17:46:00 +0800

    add

---
+++ dawang/dawang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dawang
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 王治庭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dawang/dawang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dawang
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 王治庭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
